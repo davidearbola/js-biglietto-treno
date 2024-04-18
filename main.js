b731f71ba7ea18859b002587e2d918d3ebb765b4
davidearbola <davidearbola@gmail.com>
2024-04-18 10:42:21 +0200

    fix percentuale

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 const kmCost = 0.21;
-const discount20 = 1.20;
-const discount40 = 1.40;
+const discount20 = 0.80;
+const discount40 = 0.60;
 
 function ticketCost() {
 
@@ -12,12 +12,12 @@ let ticket = (km*kmCost).toFixed(2);
 
 
 if (age < 18) {
-    let ticketDiscount20 = (ticket/discount20).toFixed(2)
+    let ticketDiscount20 = (ticket*discount20).toFixed(2)
     document.getElementById("ticket-cost").innerHTML = `Il tuo biglietto costerà: ${ticketDiscount20} €`;
 } else if (age >= 18 && age <= 65){
     document.getElementById("ticket-cost").innerHTML = `Il tuo biglietto costerà: ${ticket} €`;
 } else if (age > 65) {
-    let ticketDiscount40 = (ticket/discount40).toFixed(2)
+    let ticketDiscount40 = (ticket*discount40).toFixed(2)
     document.getElementById("ticket-cost").innerHTML = `Il tuo biglietto costerà: ${ticketDiscount40} €`;
 } else if (age == null) {
     document.getElementById("ticket-cost").innerHTML = "";
